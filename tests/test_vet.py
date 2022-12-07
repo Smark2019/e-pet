a2925b6ec462d9648b523a02e4c9326a15cfa17a
Metin Arkanöz <salih8461921@outlook.com>
2022-12-07 11:22:13 +0300

    test for edit_pet() func added.

--- tests/test_vet.py
+++ tests/test_vet.py
@@ -22,7 +22,22 @@ def test_able_to_add_pet():
     connection.close()
     return test_pet.id == result[0] # maybe other attributes shall be checked. later on we back.
 
-  
+def test_edit_pet():
+    test_pet = Pet.Pet("Olive","12.12.2021",
+    "Dog","Male","No","Healthy","12345",1)
+    
+    vet_operations.edit_pet(test_pet.id,"Yes",
+    "Not Healthy",test_pet.owner_ID)
+
+    connection = sqlite3.connect("epet_database.db")
+    cursor = connection.cursor()
+    connection.commit()
+    get_added_pet ="""SELECT * FROM pet WHERE id = ? """
+    cursor.execute(get_added_pet, (test_pet.id,))
+    result = cursor.fetchone()
+    connection.close()
+    return result[5] == "Yes" and result[6] == "Not Healthy"
+    
 
 
 
