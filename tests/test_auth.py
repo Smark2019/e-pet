c7524669f7ef19921addbbb695ff07b5e0b18251
Selim Cavas <selim.cavas@ozu.edu.tr>
2022-12-07 13:05:41 +0300

    fixed issue with auth test user was not unblocked after testing

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,6 +5,17 @@ import sqlite3
 
 # Pytest is used to run the test run with python -m pytest
 
+
+def unblock_user(id):
+    connection = sqlite3.connect("epet_database.db")
+    cursor = connection.cursor()
+    connection.commit()
+    cursor.execute("UPDATE user SET blocked_date = ? WHERE id = ?", (None, id))
+    cursor.execute("UPDATE user SET login_attempts = 0 WHERE id = ?", (id,))
+    connection.commit()
+    connection.close()
+
+
 def get_random_user_credentials():
     connection = sqlite3.connect("epet_database.db")
     cursor = connection.cursor()
@@ -15,6 +26,7 @@ def get_random_user_credentials():
     id = random[0]
     password = random[1]
     return id, password
+
 
 def test_login_response_time():
     id, password = get_random_user_credentials()
@@ -35,12 +47,16 @@ def test_successful_login():
     assert auth_operation.authentification(
         id, password) == 1
 
+
 def test_user_block_for_unsuccessful_login():
     id, password = get_random_user_credentials()
     for i in range(4):
         auth_operation.authentification(
             id, hashlib.sha256("1231".encode('utf-8')).hexdigest())
-    assert auth_operation.authentification(id, hashlib.sha256("1231".encode('utf-8')).hexdigest()) == -2 or -3
 
+    login_status = auth_operation.authentification(id, hashlib.sha256(
+        "1231".encode('utf-8')).hexdigest())
 
+    unblock_user(id)
 
+    assert login_status == -2 or -3
